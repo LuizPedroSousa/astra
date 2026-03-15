611bf2244395137da8fbeb468825e2e7bc117b72
LuizPedroSousa <luizpedrosousa64@gmail.com>
2026-03-15 10:42:30 -0300

    feat(shader-lang): wire canonical and GLSL lowering passes into compiler pipeline :sparkles:

--- src/modules/renderer/shader-lang/compiler.cpp
+++ src/modules/renderer/shader-lang/compiler.cpp
@@ -1,6 +1,8 @@
 #include "shader-lang/compiler.hpp"
 #include "shader-lang/emitters/opengl-glsl-emitter.hpp"
 #include "shader-lang/linker.hpp"
+#include "shader-lang/lowering/canonical-lowering.hpp"
+#include "shader-lang/lowering/glsl-lowering.hpp"
 #include "shader-lang/parser.hpp"
 #include "shader-lang/tokenizer.hpp"
 
@@ -49,12 +51,27 @@ CompileResult Compiler::compile(std::string_view source,
     return result;
   }
 
-  OpenGLGLSLEmitter emitter(link_result.all_nodes);
+  CanonicalLowering canonical_lowering(link_result.all_nodes);
+  GLSLLowering glsl_lowering;
+  OpenGLGLSLEmitter emitter;
   for (NodeID sid : program.stages) {
     const auto &func_decl = std::get<FuncDecl>(link_result.all_nodes[sid].data);
+    CanonicalLoweringResult canonical_result = canonical_lowering.lower(
+        program, link_result, func_decl.stage_kind.value());
+    if (!canonical_result.ok()) {
+      result.errors = canonical_result.errors;
+      return result;
+    }
+
+    GlslLoweringResult glsl_result =
+        glsl_lowering.lower(canonical_result.stage);
+    if (!glsl_result.ok()) {
+      result.errors = glsl_result.errors;
+      return result;
+    }
+
     result.stages[func_decl.stage_kind.value()] =
-        emitter.emit(program, func_decl.stage_kind.value(),
-                     link_result.uniform_usage[func_decl.stage_kind.value()]);
+        emitter.emit(glsl_result.stage);
   }
 
   return result;
