b186dc6f1655b72b268a8e06a8e882c21ae6ca4f
LuizPedroSousa <luizpedrosousa64@gmail.com>
2026-03-30 17:39:02 -0300

    chore(editor): remove the legacy tauri frontend :lipstick:

--- editor/src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import "../app.css";
-  import "@fontsource/roboto";
-  import Engine from "../contexts/engine.svelte";
-
-  let { children } = $props();
-</script>
-
-{@render children()}
