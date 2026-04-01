bca6693b7a0b9a78aee1fe11e7055f1e632e37bf
LuizPedroSousa <luizpedrosousa64@gmail.com>
2026-04-01 18:14:40 -0300

    chore(project/path-manager): drop path resolution debug logs :lipstick:

--- src/modules/project/managers/path-manager.cpp
+++ src/modules/project/managers/path-manager.cpp
@@ -14,8 +14,6 @@ PathManager::resolve_project_path(std::string relative_path) {
   auto resolved_path = std::filesystem::path(project_config.directory) /
                        project_config.resources.directory / relative_path;
 
-  LOG_DEBUG(resolved_path);
-
   return resolved_path;
 }
 
@@ -31,12 +29,10 @@ std::filesystem::path PathManager::resolve(Ref<Path> path) {
 
   switch (path->get_base_directory()) {
   case BaseDirectory::Project: {
-    LOG_DEBUG("PROJECT PATH", path->get_relative_path());
     return resolve_project_path(path->get_relative_path());
   }
 
   case BaseDirectory::Engine: {
-    LOG_DEBUG("ENGINE PATH", path->get_relative_path());
     return resolve_engine_path(path->get_relative_path());
   }
 
